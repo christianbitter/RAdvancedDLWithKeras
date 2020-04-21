59f8e796fef62439bd216ffa6f435b637a86621b
Christian Bitter <christian.bitter@xarvio.com>
2020-04-21 09:06:29 +0000

    Initial Release

---
+++ Chapter_1/cnn-model-1_3_2.R
@@ -0,0 +1,26 @@
+# 
+# A sample CNN network for classification
+# https://github.com/PacktPublishing/Advanced-Deep-Learning-with-Keras/blob/master/chapter1-keras-quick-tour/cnn-model-1.3.2.py
+
+
+rm(list = ls());
+
+library(tensorflow);
+library(keras)
+
+n_digits <- 10;
+model <- keras::keras_model_sequential();
+
+model %>%
+  keras::layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = 'relu', strides = 2,
+                       input_shape = c(28, 28, 1), padding='same') %>%
+  keras::layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation='relu', strides=2) %>%
+  keras::layer_flatten() %>%
+  keras::layer_dense(n_digits, activation='softmax');
+
+model %>% 
+  keras::compile(loss='categorical_crossentropy',
+                 optimizer = keras::optimizer_rmsprop(),
+                 metrics = c('accuracy'));
+
+summary(model);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# R Companion to Advanced Deep Learning with Keras
+
+## Introduction
+This repository is a source translation of the artifacts provided for the PACKT book "Advanced Deep Learning with Keras", which is located here:
+https://github.com/PacktPublishing/Advanced-Deep-Learning-with-Keras
